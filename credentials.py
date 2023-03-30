5e1a88e687f6c00a39f40f285f63ff52dd167f60
Devg03 <devgandhi914@gmail.com>
2023-03-30 13:46:22 -0400

    fixed password verification

--- credentials.py
+++ credentials.py
@@ -1,1 +1,24 @@
-users = []
+# Needs to check if the username is taken
+
+file = open('database.txt', "a+")
+
+
+def userName_check(userName: str) -> int:
+    content = file.read()
+
+    if userName in content:
+        return 1
+    else:
+        return 0
+
+
+def createFile(name: str, userName: str, email: str, password: str):
+    form = f'{name} = [Name: {name}, userName: {userName}, Email: {email}, Password: {password}] '
+    file.write(form + "\n")
+    file.close()
+
+
+def main(name: str, userName: str, email: str, password: str):
+    users_info = {'name': name, 'Username': userName, 'email': email, 'password': password}
+
+    createFile(name, userName, email, password)
--- main.py
+++ main.py
@@ -1,68 +1,60 @@
-def passVerify(name: str, pass1: str, pass2: str):
-    if pass1 == pass2:
-        print(f'\n\t\tWell Done, {name}\nYou have successfully created an account!')
-    else:
-        while pass1 != pass2:
-            print('Your passwords do not match.')
-            pass2 = input(f'{name}, Please enter you password again: ')
-            passVerify(name, pass1, pass2)
-
-    # TODO: Needs to add to the User's object
+import credentials
 
 
-def setPassword(name: str):
-    password1 = input('Please enter your password: ')
-    password2 = input('Please enter you password again: ')
-
-    passVerify(name, password1, password2)
+def passInfo(name: str, userName: str, email: str, password: str):
+    credentials.main(name, userName, email, password)
 
 
-def setEmail(name: str):
-    email = input(f'{name}, Please enter your email: ')
+def passVerify(password1: str, password2: str) -> bool:
+    if password1 != password2:
+        print('Your passwords do not match.')
+        pass2 = input('Please enter you password again: ')
+        passVerify(password1, pass2)
+    return True
 
-    # TODO: Needs to add it to the User's object
-    # TODO: If possible I can possibly add email verification system.
-    setPassword(name)
 
-
-def setUserName(name: str):
+def setInfo(name: str):
     userName = input(f'{name}, Please enter the user name that you wish to set for your account: ')
-    if " " in userName:
-        print("Your username should not have any spaces.")
-        setUserName(name)
+    # Checks if the username is taken or not based on 0 or anything else (Which is 1).
+    if credentials.userName_check(userName) == 0:
+        if " " in userName:
+            print("Your username should not have any spaces.")
+            setInfo(name)
+        else:
+            email = input(f'{name}, Please enter your email: ')
+
+            # TODO: If possible I can possibly add email verification system.
+            password1 = input('Please enter your password: ')
+            password2 = input('Please enter you password again: ')
+
+            if passVerify(password1, password2):
+                print(f'\n\t\tWell Done, {name}\nYou have successfully created an account!')
+
+                passInfo(name, userName, email, password1)
     else:
-        setEmail(name)
-
-    # TODO: Needs to check if this username is taken or not through iterating the credentials file
+        print(f"Please choose a different username, {userName} is taken")
 
 
-def signUp():
-    name = input('Please enter your full name: ')
-    print(f'Nice to meet you {name}.')
-
-    setUserName(name)
-    # TODO: Needs to create a hash map or an object that bundles an user information
-
-
-def login():
-    userOrEmail = input("Please enter your user name or email: ")
-    # TODO: Must setup the User's object first in order to verify if the username or email even exists
-
-    password = input("Please enter your password: ")
-    # TODO: Must check if the User's password matches the corresponding email or username
-
-
-def page():
+def main():
     # TODO: At last, add a welcoming message first and then present selection
 
     selection = input("Do you wish to 'sign up' or 'login'? ")
 
     if selection == 'Sign up':
-        signUp()
+        name = input('Please enter your full name: ')
+        print(f'Nice to meet you {name}.')
+
+        setInfo(name)
+
     elif selection == 'Login':
-        login()
+        userOrEmail = input("Please enter your user name or email: ")
+        # TODO: Must setup the User's object first in order to verify if the username or email even exists
+
+        password = input("Please enter your password: ")
+        # TODO: Must check if the User's password matches the corresponding email or username
     else:
         print('Enter a valid response.')
 
 
-page()
+if __name__ == '__main__':
+    main()
